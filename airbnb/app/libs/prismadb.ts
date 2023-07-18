bf8575df421ec879ef9573cd2900fe5982568513
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-07-18 17:04:33 +0300

    Update prismadb.ts

--- airbnb/app/libs/prismadb.ts
+++ airbnb/app/libs/prismadb.ts
@@ -11,4 +11,4 @@ if (process.env.NODE_END !== 'production') globalThis.prisma = client
 export default client;
 
 
-// see explanation in prismadb.md
+// see explanation in prismadb.md , but to prevent hot reloading from draining resources
