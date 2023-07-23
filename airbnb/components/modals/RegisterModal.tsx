438bffbcc301ce97893952ce66dbf02946d6af83
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-07-23 16:26:43 +0300

    toggle to login

--- airbnb/components/modals/RegisterModal.tsx
+++ airbnb/components/modals/RegisterModal.tsx
@@ -16,133 +16,142 @@ import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast'
 import Button from '../Button';
 import { signIn } from 'next-auth/react';
+import useLoginModal from '@/app/hooks/useLoginModal';
 
 
 
 const RegisterModal = () => {
-  
-  const registerModal = useRegisterModal();
-  const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: {
-        errors,
+    const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    })
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/register', data)
+            .then(() => {
+                registerModal.onClose();
+            })
+            .catch((err) => {
+                toast.error('Something went wrong.')
+                // cannot do toast.error(err) due to err being an object and objects are not valid as a React child => toast returns a react child, and err is an
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
     }
-  } = useForm<FieldValues>({
-    defaultValues: {
-        name: '',
-        email: '',
-        password: ''
-    }
-  })
-  
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true);
 
-    axios.post('/api/register', data)
-        .then(()=>{
-            registerModal.onClose();
-        })
-        .catch((err)=>{
-            toast.error('Something went wrong.')
-            // cannot do toast.error(err) due to err being an object and objects are not valid as a React child => toast returns a react child, and err is an
-        })
-        .finally(()=>{
-            setIsLoading(false);
-        })
-  }
-  const bodyContent = (
-    <div className='flex flex-col gap-4'>
-        <Heading 
-            title='Welcome to Airbnb'
-            subtitle='Create an account'
-        />
-        <Input 
-          id='email'
-          label='Email'
-          disabled={isLoading}
-          register={register}
-          errors={errors}
-          required
-        />
-        <Input 
-          id='name'
-          label='Name'
-          disabled={isLoading}
-          register={register}
-          errors={errors}
-          required
-        />
-        <Input 
-          id='password'
-          label='Password'
-          type='password'
-          disabled={isLoading}
-          register={register}
-          errors={errors}
-          required
-        />
-    </div>
-  )
+    const onToggle = useCallback(() => {
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [loginModal, registerModal])
 
-  const footerContent = (
-    <div
-      className='flex flex-col gap-4 mt-3'
-    >
-      <hr />
-      <Button 
-        outline
-        label="Continue with Google"
-        icon={FcGoogle}
-        onClick={()=>signIn('google')}
-      />
-      <Button 
-        outline
-        label="Continue with Github"
-        icon={AiFillGithub}
-        onClick={()=>signIn('github')}
-      />
-      <div
-        className='
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading
+                title='Welcome to Airbnb'
+                subtitle='Create an account'
+            />
+            <Input
+                id='email'
+                label='Email'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id='name'
+                label='Name'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id='password'
+                label='Password'
+                type='password'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    )
+
+    const footerContent = (
+        <div
+            className='flex flex-col gap-4 mt-3'
+        >
+            <hr />
+            <Button
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => signIn('google')}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => signIn('github')}
+            />
+            <div
+                className='
           text-neutral-500
           text-center
           mt-4
           font-light
         '
-      >
-        <div className='justify-center flex flex-row items-center gap-3'>
-          <div>
-            Already have an account?
-          </div>
-          <div
-            onClick={registerModal.onClose}
-            className='
+            >
+                <div className='justify-center flex flex-row items-center gap-3'>
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div
+                        onClick={onToggle}
+                        className='
               text-neutral-800
               cursor-pointer
               hover:underline
             '
-          >
-            Log in
-          </div>
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 
-  return (
-    <Modal 
-        disabled={isLoading}
-        isOpen={registerModal.isOpen}
-        title="Register"
-        actionLabel='Continue'
-        onClose={registerModal.onClose}
-        onSubmit={handleSubmit(onSubmit)} // useForm requires handleSubmit wrapper around your function
-        body={bodyContent}
-        footer={footerContent}
-    ></Modal> //disabled becomes true when we are loading something, thus, awaiting
-  )
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title="Register"
+            actionLabel='Continue'
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)} // useForm requires handleSubmit wrapper around your function
+            body={bodyContent}
+            footer={footerContent}
+        ></Modal> //disabled becomes true when we are loading something, thus, awaiting
+    )
 }
 
 export default RegisterModal
