ad3a4cf69e6e39a68661ae93c6ccaa48be00d61d
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-07-27 11:23:49 +0300

    listing start

--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -32,7 +32,9 @@ export default async function RootLayout({
         <RegisterModal></RegisterModal>
         <LoginModal></LoginModal>
         <Navbar currentUser={currentUser}></Navbar>
-        {children}
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
       </body>
     </html>
   )
