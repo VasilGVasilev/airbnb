c3b52c3bbd02fcf03152dfa0502ec51e28c1acbd
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-07-28 20:04:43 +0300

    favourite init

---
+++ airbnb/app/api/favourites/[listingId]/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb"
+
+interface IParams {
+    listingsId?: string;
+}
+
+export async function POST(
+    request: Request,
+    { params }: {params: IParams}
+){
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return NextResponse.error();
+    }
+    
+    const { listingsId } = params;
+
+    if(!listingsId || typeof listingsId !== 'string'){
+        throw new Error('Invalid ID');
+    }
+
+    let favoriteIds = [...(currentUser.favouriteIds || [])]
+
+}
+
+// NB BEST PRACTICE TO clone arrays when copying    let favoriteIds = [...(currentUser.favouriteIds || [])]
+// (currentUser.favouriteIds || []) will check if first arg is truthy and if so use its value, if not use an empty array value
+// the spread syntax is necessary so that favoriteIds are a copy of the array, not a reference,
+// BUT mind that spread syntax is making a shallow copy. see more on shallow vs deep clone
+// also see https://dev.to/samanthaming/how-to-deep-clone-an-array-in-javascript-3cig
