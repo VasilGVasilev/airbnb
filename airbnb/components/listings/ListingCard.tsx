ad7b8569d7c5f4fff50fd09e4a630dbcb5bcbb7f
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-07-28 14:13:09 +0300

    listings get all ready

--- airbnb/components/listings/ListingCard.tsx
+++ airbnb/components/listings/ListingCard.tsx
@@ -7,6 +7,9 @@ import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
     data: Listing; //cool that prisma client provides us with the object out of the model 
@@ -64,10 +67,71 @@ const ListingCard: React.FC<ListingCardProps> = ({
     }, [reservation])
      
     return (
-        <div>
-
+        <div
+            onClick={() => router.push(`/listings/${data.id}`)}
+            className="
+                col-span-1 cursor-pointer group
+            "
+        >
+            <div className="flex flex-col gap-2 w-full">
+                <div
+                    className="
+                        aspect-square
+                        w-full
+                        relative
+                        overflow-hidden
+                        rounded-xl
+                    "
+                >
+                    <Image
+                        alt="Listing"
+                        src={data.imageSrc}
+                        fill
+                        className="
+                            object-cover
+                            h-full
+                            w-full
+                            group-hover:scale-110
+                            transition
+                        " 
+                    />
+                    <div className="absolute top-3 right-3">
+                        <HeartButton 
+                            listingId={data.id}
+                            currentUser={currentUser}
+                        />
+                    </div>
+                </div>
+                <div className="font-semibold text-lg">
+                    {location?.region}, {location?.label}
+                </div>
+                <div className="font-light text-neutral-500">
+                    {reservationDate || data.category}
+                </div>
+                <div className="flex flex-row items-center gap-1">
+                    <div className="font-semibold">
+                        $ {price}
+                    </div>
+                    {!reservation && (
+                        <div className="font-light">
+                            night
+                        </div>
+                    )}
+                </div>
+                {onAction && actionLabel && (
+                    <Button
+                        disabled={disabled}
+                        small
+                        label={actionLabel}
+                        onClick={handleCancel}
+                    />
+                )}
+            </div>
         </div>
     )
 }
 
 export default ListingCard
+
+
+// NB group: when we hover on any part of the listing card which has a group tag, we will enlarge the image with group:hover tag
