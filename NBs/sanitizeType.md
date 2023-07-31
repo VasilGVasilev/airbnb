a119a2a35dde63a14e98902ad267d551e3f2fc85
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-07-31 18:39:54 +0300

    ListingHeading

--- NBs/sanitizeType.md
+++ NBs/sanitizeType.md
@@ -1,3 +1,5 @@
+Especially important since there is no universal standrd for DB types and the types of our programming language. We may have entirely separate teams FE, BE, DB, developing in technologies of different ecosystems, thus, the necessity to translate types via sanitization.
+
 ```sh
 import { User } from "@prisma/client";
 
--- airbnb/app/listings/[listingId]/ListingClient.tsx
+++ airbnb/app/listings/[listingId]/ListingClient.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import { SafeListing, SafeUser } from '@/app/types';
+import Container from '@/components/Container';
+import ListingHead from '@/components/listings/ListingHead';
+import { categories } from '@/components/navbar/Categories';
 import { Reservation } from '@prisma/client';
-import React from 'react'
+import { useMemo } from 'react'
 
 interface ListingClientProps{
     reservations?: Reservation[];
@@ -15,15 +20,35 @@ const ListingClient: React.FC<ListingClientProps> = ({
     listing,
     currentUser
 }) => {
+
+    const category = useMemo(()=>{
+        return categories.find((item)=> item.label === listing.category)
+    }, [listing.category]); //match DB category with default categories stored in a const
+
     return (
-        <div>
-            Listing client
-        </div>
+        <Container>
+            <div className='max-w-screen-lg mx-auto'>
+                <div className='flex flex-col gap-6'>
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </Container>
     )
 }
 
 export default ListingClient
 
+// NB
+
 // & in a type position means intersection type
 // https://www.typescriptlang.org/docs/handbook/2/objects.html#intersection-types
 // https://stackoverflow.com/questions/38317625/what-does-the-ampersand-mean-in-a-typescript-type-definition
+
+// max-width
+// If the image is larger than its parent element. By using max-width: 100% , the width of the image won't exceed the width of its parent.
