af0a18219756b16ce2fa6db1ee53a345e65831f0
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-08-03 14:19:11 +0300

    reservations setup

--- airbnb/app/listings/[listingId]/ListingClient.tsx
+++ airbnb/app/listings/[listingId]/ListingClient.tsx
@@ -1,12 +1,21 @@
 'use client'
 
+import useLoginModal from '@/app/hooks/useLoginModal';
 import { SafeListing, SafeUser } from '@/app/types';
 import Container from '@/components/Container';
 import ListingHead from '@/components/listings/ListingHead';
 import ListingInfo from '@/components/listings/ListingInfo';
 import { categories } from '@/components/navbar/Categories';
 import { Reservation } from '@prisma/client';
+import { eachDayOfInterval } from 'date-fns'; //https://date-fns.org/v2.30.0/docs/eachDayOfInterval
+import { useRouter } from 'next/navigation';
 import { useMemo } from 'react'
+
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+};
 
 interface ListingClientProps{
     reservations?: Reservation[];
@@ -19,8 +28,23 @@ interface ListingClientProps{
 
 const ListingClient: React.FC<ListingClientProps> = ({
     listing,
+    reservations = [], //set a default value to be an array so that you can safely run .find(), .map() without errors
     currentUser
 }) => {
+
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const disabledDates = useMemo(()=>{
+        let dates: Date[] = [];
+
+        reservations.forEach((reservation: any) => {
+            const range = eachDayOfInterval({
+                start: new Date(reservation.startDate),
+                end: new Date(reservation.endDate)
+            })
+        })
+    },[])
 
     const category = useMemo(()=>{
         return categories.find((item)=> item.label === listing.category)
@@ -48,11 +72,11 @@ const ListingClient: React.FC<ListingClientProps> = ({
                     >
                         <ListingInfo
                             user={listing.user}
-                            category={category}
                             description={listing.description}
                             roomCount={listing.roomCount}
                             guestCount={listing.guestCount}
                             bathroomCount={listing.bathroomCount}
+                            category={category}
                             locationValue={listing.locationValue}
                         />
                     </div>
