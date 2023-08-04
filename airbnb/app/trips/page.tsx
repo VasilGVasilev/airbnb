7eb735df3123d31032d0aaa66e518c9a4c97b30a
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-08-05 00:04:14 +0300

    trips page setup

---
+++ airbnb/app/trips/page.tsx
@@ -0,0 +1,17 @@
+import EmptyState from "@/components/EmptyState";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+const TripsPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return (
+            <EmptyState
+                title=""
+                subtitle=""
+            />
+        )
+    }
+}
