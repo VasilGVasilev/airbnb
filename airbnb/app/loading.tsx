20376db4d9ba1bb74e3858c8d7ed6a77d970ff4f
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-08-10 16:18:50 +0300

    Create loading.tsx

---
+++ airbnb/app/loading.tsx
@@ -0,0 +1,13 @@
+import Loader from "@/components/Loader";
+
+const Loading = () => {
+    return (
+        <Loader />
+    )
+}
+
+export default Loading;
+
+// NB
+
+// Loading is for inbetween loading, not inital load
